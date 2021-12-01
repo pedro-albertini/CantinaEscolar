20181bf5dfd124a934df72dffa0b634df8f9aa28
Pedro Albertini <88116673+pedro-albertini@users.noreply.github.com>
2021-11-30 21:26:43 -0300

    Add files via upload

---
+++ CantinaEscolar/CantinaEscolar/Aluno.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LibraryCantinaEscolar;
+
+namespace CantinaEscolar
+{
+    class Aluno
+    {
+        public string nome;
+        public double idade;
+        public string turma;
+        public double ra;
+
+        public void cadastrarAluno()
+        {
+            Console.WriteLine("Qual o seu nome: ");
+            nome = Console.ReadLine();
+            Console.WriteLine("Qual a sua idade: ");
+            idade = double.Parse(Console.ReadLine());
+            Console.WriteLine("Qual a sua turma: (ex: primeiro ano)");
+            turma = Console.ReadLine();
+            Console.WriteLine("Qual o seu RA escolar: ");
+            ra = double.Parse(Console.ReadLine());
+        }
+
+        public void mostrarCadastro()
+        {
+            Console.WriteLine("--- CADASTRO ---");
+            Console.WriteLine("Nome:{0}", nome);
+            Console.WriteLine("Idade:{0}", idade);
+            Console.WriteLine("Turma:{0}", turma);
+            Console.WriteLine("RA:{0}", ra);
+        }
+
+        public void sairFila()
+        {
+            Console.Clear();
+        }
+    }
+}
---
+++ CantinaEscolar/CantinaEscolar/Conexao.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Data;
+using LibraryCantinaEscolar;
+
+namespace CantinaEscolar
+{
+    class Conexao
+    {
+        public string ra;
+        public int idade;
+        public string turma;
+
+
+        public void cadastroAluno()
+        {
+            MySqlConnection conexao;
+            conexao = new MySqlConnection("server=localhost;database=cantina;uid=root;password=vertrigo");
+            try
+            {
+                conexao.Open();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message.ToString());
+                Console.WriteLine("Não foi possível conectar com o banco de dados");
+                Console.ReadKey();
+                Environment.Exit(0);
+            }
+
+
+
+            MySqlCommand cmd;
+            Console.WriteLine("Digite um ra para ser cadastrado");
+            ra = Console.ReadLine();
+            Console.WriteLine("Digite uma idade para este usuario");
+            idade = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite turma para este usuario");
+            turma = Console.ReadLine();
+            string sql;
+            //insert into login values ('usuario', 'senha', 'email');
+            //sql = "insert into login values ('" + usuario + "','" + senha + "','" + email + "')";
+            //Console.WriteLine("SQL:" + sql);
+            sql = "insert into aluno values(@ra,@idade,@turma)";
+            cmd = new MySqlCommand(sql, conexao);
+            cmd.Parameters.AddWithValue("@ra", ra);
+            cmd.Parameters.AddWithValue("@idade", idade);
+            cmd.Parameters.AddWithValue("@turma", turma);
+            cmd.ExecuteNonQuery();
+            conexao.Close();
+
+            Console.ReadKey();
+
+        }
+
+        public void atualizarAluno()
+        {
+            MySqlConnection conexao;
+            conexao = new MySqlConnection("server=localhost;database=cantina;uid=root;password=vertrigo");
+            try
+            {
+                conexao.Open();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message.ToString());
+                Console.WriteLine("Não foi possível conectar com o banco de dados");
+                Console.ReadKey();
+                Environment.Exit(0);
+            }
+
+
+
+            MySqlCommand cmd;
+            string sqlupdate = "update aluno set idade=@idade, turma@turma where ra=@ra";
+            cmd = new MySqlCommand(sqlupdate, conexao);
+            cmd.Parameters.AddWithValue("@ra", ra);
+            cmd.Parameters.AddWithValue("@idade", idade);
+            cmd.Parameters.AddWithValue("@turma", turma);
+            cmd.ExecuteNonQuery();
+            conexao.Close();
+
+
+            Console.ReadKey();
+        }
+
+        public void verAluno()
+        {
+            MySqlConnection conexao;
+            conexao = new MySqlConnection("server=localhost;database=cantina;uid=root;password=vertrigo");
+            try
+            {
+                conexao.Open();
+            }
+            catch (Exception e)
+            {
+                //Console.WriteLine(e.Message.ToString());
+                Console.WriteLine("Não foi possível conectar com o banco de dados");
+                Console.ReadKey();
+                Environment.Exit(0);
+            }
+
+
+
+            MySqlCommand cmd;
+            string sqlselect = "select * from aluno where ra = @ra";
+            cmd = new MySqlCommand(sqlselect, conexao);
+            cmd.Parameters.AddWithValue("@ra", ra);
+            cmd.CommandText = sqlselect;
+            MySqlDataReader resultado = cmd.ExecuteReader();
+
+            while (resultado.Read())
+            {
+                Console.WriteLine("RA: " + resultado["ra"]);
+                Console.WriteLine("Idade: " + resultado["idade"]);
+                Console.WriteLine("Turma: " + resultado["turma"]);
+            }
+            conexao.Close();
+
+            Console.ReadKey();
+        }
+
+        public void deletarAluno()
+        {
+            MySqlConnection conexao;
+            conexao = new MySqlConnection("server=localhost;database=cantina;uid=root;password=vertrigo");
+            try
+            {
+                conexao.Open();
+            }
+            catch (Exception e)
+            {
+                //Console.WriteLine(e.Message.ToString());
+                Console.WriteLine("Não foi possível conectar com o banco de dados");
+                Console.ReadKey();
+                Environment.Exit(0);
+            }
+
+
+
+            MySqlCommand cmd;
+            string sqldelete = "delete from aluno where ra=@ra";
+            cmd = new MySqlCommand(sqldelete, conexao);
+            cmd.Parameters.AddWithValue("@ra", ra);
+            cmd.CommandText = sqldelete;
+            cmd.ExecuteNonQuery();
+            conexao.Close();
+
+
+            Console.ReadKey();
+        }
+    }
+}
